55ac54ac85fc65c7c6e308b8bf9000c80154ca5f
drice82 <xujiabin82@gmail.com>
2017-04-05 20:09:26 +0800

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,51 @@
+<?php include "inc.php";?>
+<!DOCTYPE html>
+<html lang="en_US">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>LazyPT</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+<link href="./css" rel="stylesheet" type="text/css">
+<link href="./style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<header>
+<h1>LazyPT</h1>
+<p>Enjoy PT!</p>
+</header>
+
+<main>
+<section>
+<img src="./utorrent.png">
+<div>
+<h2>RuTorrent</h2>
+<p><a href="./rutorrent/">PT下载工具</a></p>
+</div>
+</section>
+
+<section>
+<img src="./explorer.png">
+<div>
+<h2>File explorer</h2>
+<p><a href="./Downloads/">浏览文件</a></p>
+</div>
+</section>
+
+<section>
+<img src="./recharge.png">
+<img src="./recharge.png">
+<div>
+<h2>Recharge</h2>
+<p><a href="https://www.google.com/" target="_blank">充值缴费</a></p>
+</div>
+</section>
+</main>
+
+<footer>
+<p>©2016 - 2017 <br> <a href="http://ip100.info/">IP100.info</a> | <a href="https://www.google.com/">Blog</a> | <a href="https://www.google.com">Google</a></p>
+</footer>
+</body>
+</html>
